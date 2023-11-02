1706341eb2a2bedcda8baf3fa9202f46aea0f8d0
SaafiRahman <saafirah@student.ubc.ca>
2023-11-02 12:59:15 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React from 'react';
+import Timer from './Timer'
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [reset,  setReset] = useState(false);
+
+  const startTimer = () => {
+    setIsRunning(true);
+  };
+
+  const pauseTimer = () => {
+    setIsRunning(false);
+  }
+
+  const resetTimer = () => {
+    setReset(true);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="title-text">
+          POMODORO TIMER
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div>
+          <Timer isrunning={isRunning} reset={reset}/> 
+      </div>
+
+      <div className='buttons'>
+        <div className="button" onClick={startTimer}>
+          <p>play</p>
+        </div>
+
+        <div className="button" onClick={pauseTimer}>
+          <p>pause</p>
+        </div>
+
+        <div className="button" onClick={resetTimer}>
+          <p>reset</p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Timer.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+
+function Timer({isrunning, reset}) {
+    const [minutes, setMinutes] = useState(1);
+    const [seconds, setSeconds] = useState(0);
+    const [start, setStart] = useState(false);
+
+    useEffect(() => { 
+            setStart(isrunning);
+    }, [isrunning])
+
+    useEffect(() => {
+            console.log("also resetted");
+            setStart(false);
+            setMinutes(1);
+            setSeconds(0);
+    }, [reset])
+
+    useEffect(() => {   
+        let interval;
+        if  (start && (minutes > 0 || seconds > 0)) {
+            interval = setInterval(() => {
+                if (seconds > 0) {
+                    setSeconds((seconds) => seconds - 1);
+                } else if (minutes > 0) {
+                    setMinutes((minutes) =>  minutes - 1);
+                    setSeconds(59);
+                }
+            }, 1000)
+        } 
+        return () => clearInterval(interval);
+
+    }, [seconds, minutes, start])
+    
+    return (
+        <div>
+            <h1>{minutes} : {seconds}</h1>
+        </div>
+    );
+}
+export default Timer;
